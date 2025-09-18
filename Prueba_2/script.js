f78f42b42edcf134629ba54515753050d14e85da
jmm96-13 <jmm.cv.96@gmail.com>
2025-09-18 19:40:06 +0200

    pruebas

--- Prueba_2/script.js
+++ Prueba_2/script.js
@@ -1,6 +1,9 @@
 const nombreUsuario = prompt("Introduce tu nombre:");
 const edadUsuario = prompt("Introduce tu edad:");
 
+  // cuando una variable se transforma a una constante? cuando ya no se puede cambiar
+  //constantesç. variables que obtienen un valor fijo **/
+
 alert("Hola, " + nombreUsuario + ". Tienes " + edadUsuario + " años.");
 
 document.getElementById("saludo").innerHTML =
